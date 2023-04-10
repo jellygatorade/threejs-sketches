eec2b331a997b5d644f6516e88322450f44262ab
Kevin Kane <kkane@ncartmuseum.org>
2023-04-09 21:56:38 -0400

    add l-systems sketch

---
+++ sketches/lindenmayer-systems/script/downloadJSON.js
@@ -0,0 +1,19 @@
+function downloadJSON(json, filename) {
+  saveString(JSON.stringify(json), filename);
+}
+
+var link = document.createElement("a");
+link.style.display = "none";
+document.body.appendChild(link); // Firefox workaround, see #6594
+
+function save(blob, filename) {
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
+
+  // URL.revokeObjectURL( url ); breaks Firefox...
+}
+
+function saveString(text, filename) {
+  save(new Blob([text], { type: "text/plain" }), filename);
+}
